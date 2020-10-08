0728c95ffa6ed68665dbd3c7c79d3075d0a5917c
Alex Melnichuk <alex.melnichuk@pragma-technologies.com>
2020-10-08 10:02:08 +0300

    Update Mintable contract

--- contracts/IMintableERC20.sol
+++ contracts/IMintableERC20.sol
@@ -8,16 +8,6 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IMintableERC20 is IERC20 {
-
-    function mintAmount(address[] calldata accounts, uint256 amount) external;
-
-    function mintAmounts(address[] calldata accounts, uint256[] calldata amounts) external;
-    
-    function addMaintainer(address maintainer) external;
-    
-    function removeMaintainer(address maintainer) external;
-    
-    function maintainers() external view returns (address[] memory);
-
-    function maxMintedAmount() external view returns (uint256);
+    function mint(address account, uint256 amount) external;
+    function burn(address account, uint256 amount) external;
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,30 @@
 import { ethers } from '@nomiclabs/buidler';
 import { MintableErc20Factory } from '../typechain/MintableErc20Factory';
-import { ContractDetails } from '../test/shared/ContractDetails';
+import { BigNumber } from 'ethers/utils';
 
 async function main() {
   const account = (await ethers.getSigners())[0];
-  const contractDetails = new ContractDetails();
-  const mintable = await new MintableErc20Factory(account).deploy(
-    contractDetails.name,
-    contractDetails.symbol,
-    contractDetails.totalSupply,
-    contractDetails.decimals,
-    contractDetails.maxMintedSupply,
-  );
+  const name = process.env.CONTRACT_NAME;
+  if (!name) {
+    throw new Error('Unset CONTRACT_NAME');
+  }
+  const symbol = process.env.CONTRACT_SYMBOL;
+  if (!symbol) {
+    throw new Error('Unset CONTRACT_SYMBOL');
+  }
+  const totalSupplyString = process.env.CONTRACT_TOTAL_SUPPLY_IN_WEI;
+  if (!totalSupplyString) {
+    throw new Error('Unset CONTRACT_TOTAL_SUPPLY');
+  }
+  const totalSupply = new BigNumber(totalSupplyString);
+
+  const decimalsString = process.env.CONTRACT_DECIMALS;
+  if (!decimalsString) {
+    throw new Error('Unset CONTRACT_DECIMALS');
+  }
+  const decimals = Number.parseInt(decimalsString);
+
+  const mintable = await new MintableErc20Factory(account).deploy(name, symbol, totalSupply, decimals);
   console.log('Deployed MintableERC20 contract to:', mintable.address);
 }
 
